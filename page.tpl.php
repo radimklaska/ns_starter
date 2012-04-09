da27384edc58f279c75362d45eda7fc60b926970
Radim Klaska <radim@klaska.net>
2012-04-09 21:05:29 +0200

    basic D7 port

--- page.tpl.php
+++ page.tpl.php
@@ -1,108 +1,84 @@
-<?php
-// $Id: page.tpl.php,v 1.1.2.1 2009/02/24 15:34:45 dvessel Exp $
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+<div id="page" class="container-16 clearfix">
 
-<head>
-  <title><?php print $head_title; ?></title>
-  <?php print $head; ?>
-  <?php print $styles; ?>
-  <?php print $scripts; ?>
-</head>
-
-<body class="<?php print $body_classes; ?> show-grid">
-  <div id="page" class="container-16 clear-block">
-
-    <div id="site-header" class="clear-block">
-      <div id="branding" class="grid-4 clear-block">
-      <?php if ($linked_logo_img): ?>
-        <span id="logo" class="grid-1 alpha"><?php print $linked_logo_img; ?></span>
-      <?php endif; ?>
-      <?php if ($linked_site_name): ?>
-        <h1 id="site-name" class="grid-3 omega"><?php print $linked_site_name; ?></h1>
-      <?php endif; ?>
-      <?php if ($site_slogan): ?>
-        <div id="site-slogan" class="grid-3 omega"><?php print $site_slogan; ?></div>
-      <?php endif; ?>
-      </div>
-
-    <?php if ($main_menu_links || $secondary_menu_links): ?>
-      <div id="site-menu" class="grid-12">
-        <?php print $main_menu_links; ?>
-        <?php print $secondary_menu_links; ?>
-      </div>
+  <div id="site-header" class="clearfix">
+    <div id="branding" class="grid-4 clearfix">
+    <?php if ($linked_logo_img): ?>
+      <span id="logo" class="grid-1 alpha"><?php print $linked_logo_img; ?></span>
     <?php endif; ?>
-
-    <?php if ($search_box): ?>
-      <div id="search-box" class="grid-6 prefix-10"><?php print $search_box; ?></div>
+    <?php if ($linked_site_name): ?>
+      <h1 id="site-name" class="grid-3 omega"><?php print $linked_site_name; ?></h1>
+    <?php endif; ?>
+    <?php if ($site_slogan): ?>
+      <div id="site-slogan" class="grid-3 omega"><?php print $site_slogan; ?></div>
     <?php endif; ?>
     </div>
 
-
-    <div id="site-subheader" class="prefix-1 suffix-1 clear-block">
-    <?php if ($mission): ?>
-      <div id="mission" class="<?php print ns('grid-14', $header, 7); ?>">
-        <?php print $mission; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($header): ?>
-      <div id="header-region" class="region <?php print ns('grid-14', $mission, 7); ?> clear-block">
-        <?php print $header; ?>
-      </div>
-    <?php endif; ?>
-    </div>
-
-
-    <div id="main" class="column <?php print ns('grid-16', $left, 4, $right, 3) . ' ' . ns('push-4', !$left, 4); ?>">
-      <?php print $breadcrumb; ?>
-      <?php if ($title): ?>
-        <h1 class="title" id="page-title"><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php if ($tabs): ?>
-        <div class="tabs"><?php print $tabs; ?></div>
-      <?php endif; ?>
-      <?php print $messages; ?>
-      <?php print $help; ?>
-
-      <div id="main-content" class="region clear-block">
-        <?php print $content; ?>
-      </div>
-
-      <?php print $feed_icons; ?>
-    </div>
-
-  <?php if ($left): ?>
-    <div id="sidebar-left" class="column sidebar region grid-4 <?php print ns('pull-12', $right, 3); ?>">
-      <?php print $left; ?>
+  <?php if ($main_menu_links || $secondary_menu_links): ?>
+    <div id="site-menu" class="grid-12">
+      <?php print $main_menu_links; ?>
+      <?php print $secondary_menu_links; ?>
     </div>
   <?php endif; ?>
 
-  <?php if ($right): ?>
-    <div id="sidebar-right" class="column sidebar region grid-3">
-      <?php print $right; ?>
+  <?php if ($page['search_box']): ?>
+    <div id="search-box" class="grid-6 prefix-10"><?php print render($page['search_box']); ?></div>
+  <?php endif; ?>
+  </div>
+
+  <div id="site-subheader" class="prefix-1 suffix-1 clearfix">
+  <?php if ($page['highlighted']): ?>
+    <div id="highlighted" class="<?php print ns('grid-14', $page['header'], 7); ?>">
+      <?php print render($page['highlighted']); ?>
     </div>
   <?php endif; ?>
+
+  <?php if ($page['header']): ?>
+    <div id="header-region" class="region <?php print ns('grid-14', $page['highlighted'], 7); ?> clearfix">
+      <?php print render($page['header']); ?>
+    </div>
+  <?php endif; ?>
+  </div>
+
+
+  <div id="main" class="column <?php print ns('grid-16', $page['sidebar_first'], 4, $page['sidebar_second'], 3) . ' ' . ns('push-4', !$page['sidebar_first'], 4); ?>">
+    <?php print $breadcrumb; ?>
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?>
+      <h1 class="title" id="page-title"><?php print $title; ?></h1>
+    <?php endif; ?>
+    <?php print render($title_suffix); ?>      
+    <?php if ($tabs): ?>
+      <div class="tabs"><?php print render($tabs); ?></div>
+    <?php endif; ?>
+    <?php print $messages; ?>
+    <?php print render($page['help']); ?>
+
+    <div id="main-content" class="region clearfix">
+      <?php print render($page['content']); ?>
+    </div>
+
+    <?php print $feed_icons; ?>
+  </div>
+
+<?php if ($page['sidebar_first']): ?>
+  <div id="sidebar-left" class="column sidebar region grid-4 <?php print ns('pull-12', $page['sidebar_second'], 3); ?>">
+    <?php print render($page['sidebar_first']); ?>
+  </div>
+<?php endif; ?>
+
+<?php if ($page['sidebar_second']): ?>
+  <div id="sidebar-right" class="column sidebar region grid-3">
+    <?php print render($page['sidebar_second']); ?>
+  </div>
+<?php endif; ?>
 
 
   <div id="footer" class="prefix-1 suffix-1">
-    <?php if ($footer): ?>
-      <div id="footer-region" class="region grid-14 clear-block">
-        <?php print $footer; ?>
-      </div>
-    <?php endif; ?>
-
-    <?php if ($footer_message): ?>
-      <div id="footer-message" class="grid-14">
-        <?php print $footer_message; ?>
+    <?php if ($page['footer']): ?>
+      <div id="footer-region" class="region grid-14 clearfix">
+        <?php print render($page['footer']); ?>
       </div>
     <?php endif; ?>
   </div>
 
-
-  </div>
-  <?php print $closure; ?>
-</body>
-</html>
+</div>
